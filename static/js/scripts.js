3f611cbaca7d205300bc95d088bd937e6b3f9a8b
Mark Lysenko <oomarkizoo@mail.ru>
2020-09-23 23:59:13 +0300

    Added functionality for adding cards

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,13 @@
 
 $( ".customization" ).click(function() {
-    $(".popup").css({"visibility":"visible"});
-    $(".popup__customization").css({"visibility":"visible"});
+    $(".popup").css({"display":"block"});
+    $(".popup__customization").css({"display":"block"});
 });
 
 
 function closePopUp() {
-    $(".popup").css({"visibility":"hidden"});
-    $(".popup > *").css({"visibility":"hidden"});
+    $(".popup").css({"display":"none"});
+    $(".popup > *").css({"display":"none"});
 }
 
 
@@ -28,5 +28,29 @@ $( ".customization-form" ).submit(function(e) {
     }
 
     closePopUp()
+});
 
+var colForAddCard;
+
+$( ".column__add-card" ).click(function() {
+    $(".popup").css({"display":"block"});
+    $(".popup__add-card").css({"display":"block"});
+    colForAddCard = $(this);
+});
+
+
+$( ".add-card-form" ).submit(function(e) {
+    e.preventDefault();
+    const $form = $(this);
+    $form.find('.error').remove();
+
+    if ($form.find('input[id="add_card_field"]').val() !== '') {
+        let cardName = $form.find('input[id="add_card_field"]').val();
+        $(colForAddCard).siblings('.column__list-cards').append($('<a>', {
+            'text': cardName,
+            'class': 'column__card card'
+        }));
+    }
+
+    closePopUp()
 });
