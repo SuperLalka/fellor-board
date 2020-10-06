48926937bc33ac7e0d4ffd827377c8948f11793d
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-06 18:51:26 +0300

    Updates the content of the admin.py & project settings

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Boards)
+class BoardsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'bg_color']
+
+
+@admin.register(models.Columns)
+class ColumnsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Cards)
+class CardsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ['id']
