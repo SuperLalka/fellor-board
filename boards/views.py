cb6628d6a90de54208d18be1ae84e75faf50f666
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-07 23:03:09 +0300

    Combines the logic of adding/copying & renaming/moving board objects
    Also changes the response type if success

--- boards/views.py
+++ boards/views.py
@@ -12,7 +12,7 @@ def main_page(request, **kwargs):
 def boards(request):
     if request.method == "POST":
         Boards.objects.get_or_create(name=request.POST['board_name'])
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=201)
 
     elif request.method == "GET":
         boards_list = from_object_to_json(Boards.objects.all(), 'board')
@@ -30,95 +30,88 @@ def concrete_board(request, board_id):
         return JsonResponse({
             'board_columns': board_columns,
             'board_cards': board_cards,
-            'board_bg_color': board_bg_color},
-            status=200
-        )
+            'board_bg_color': board_bg_color
+        }, status=200)
 
     elif request.method == "POST":
         board = Boards.objects.get(id=board_id)
 
         if request.POST.get('name', False):
             board.name = request.POST['name']
+
         if request.POST.get('bg_color', False):
             board.bg_color = request.POST['bg_color']
 
         board.save()
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=200)
 
     elif request.method == "DELETE":
         Boards.objects.get(id=board_id).delete()
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=204)
 
 
 def columns(request):
     if request.method == "POST":
-        if request.POST['operation'] == 'add':
-            Columns.objects.get_or_create(
-                name=request.POST['column_name'],
-                board_id=request.POST['board_id']
-            )
-            return JsonResponse({"success": True}, status=200)
+        new_column = Columns.objects.create(
+            name=request.POST['name'],
+            board_id=request.POST['board_id']
+        )
 
-        elif request.POST['operation'] == 'copy':
-            column = Columns.objects.get(id=request.POST['column_id'])
-            new_column = Columns.objects.create(name=column.name, board_id=column.board_id)
-            column_cards = Cards.objects.filter(column_id=request.POST['column_id'])
+        if request.POST.get('id') != 'false':
+            column_cards = Cards.objects.filter(column_id=request.POST['id'])
 
             for card in column_cards:
                 Cards.objects.create(name=card.name, column_id=new_column.id)
-            return JsonResponse({"success": True}, status=200)
+
+        return HttpResponse(status=201)
 
 
 def concrete_column(request, column_id):
     if request.method == "POST":
         column = Columns.objects.get(id=column_id)
-        column.name = request.POST['new_name']
+
+        if request.POST.get('name'):
+            column.name = request.POST['name']
+
         column.save()
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=200)
 
     elif request.method == "DELETE":
         Columns.objects.get(id=column_id).delete()
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=204)
 
 
 def cards(request):
     if request.method == "POST":
-        if request.POST['operation'] == 'add':
-            Cards.objects.get_or_create(
-                name=request.POST['card_name'],
-                column_id=request.POST['column_id']
-            )
-            return JsonResponse({"success": True}, status=200)
-
-        elif request.POST['operation'] == 'copy':
-            card = Cards.objects.get(id=request.POST['card_id'])
-            Cards.objects.create(name=card.name, column_id=card.column_id)
-            return JsonResponse({"success": True}, status=200)
+        Cards.objects.create(
+            name=request.POST['name'],
+            column_id=request.POST['column_id']
+        )
+        return HttpResponse(status=201)
 
 
 def concrete_card(request, card_id):
     if request.method == "POST":
         card = Cards.objects.get(id=card_id)
 
-        if request.POST['operation'] == 'rename':
-            card.name = request.POST['new_name']
-            card.save()
-            return JsonResponse({"success": True}, status=200)
+        if request.POST.get('name'):
+            card.name = request.POST['name']
 
-        elif request.POST['operation'] == 'move':
+        if request.POST.get('column_id'):
             card.column_id = request.POST['column_id']
-            card.save()
-            return JsonResponse({"success": True}, status=200)
+
+        card.save()
+        return HttpResponse(status=200)
 
     elif request.method == "DELETE":
         Cards.objects.get(id=card_id).delete()
-        return JsonResponse({"success": True}, status=200)
+        return HttpResponse(status=204)
 
 
 def comments(request, card_id):
     if request.method == "POST":
-        Comments.objects.create(card_id=card_id, text=request.POST['comment_text'])
-        return JsonResponse({"success": True}, status=200)
+        Comments.objects.create(card_id=card_id, text=request.POST['text'])
+        return HttpResponse(status=201)
 
     elif request.method == "GET":
         comments_list = from_object_to_json(Comments.objects.filter(card_id=card_id), 'comment')
