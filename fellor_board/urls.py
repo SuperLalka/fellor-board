0e0fdbeb90c5b336363667adf630e7a4cf7efb7d
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-07 13:07:03 +0300

    Optimizes url patterns

--- fellor_board/urls.py
+++ fellor_board/urls.py
@@ -2,13 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fellor-boards.ru/', include('boards.urls')),
-    path('', RedirectView.as_view(url='/fellor-boards.ru/', permanent=True)),
+    path('', include('boards.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
