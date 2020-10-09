9126e87cb9cac5661098633bfb0c7ac657e2cd23
Mark Lysenko <oomarkizoo@mail.ru>
2020-10-09 12:14:50 +0300

    Makes changes to url.py to work with DRF

--- boards/urls.py
+++ boards/urls.py
@@ -5,14 +5,14 @@ from . import views
 
 
 operations = [
-    url(r'^boards$', views.boards, name='boards'),
-    url(r'^boards/(?P<board_id>\w+)$', views.concrete_board, name='concrete_board'),
-    url(r'^columns$', views.columns, name='columns'),
-    url(r'^columns/(?P<column_id>\w+)$', views.concrete_column, name='concrete_column'),
-    url(r'^cards$', views.cards, name='cards'),
-    url(r'^cards/(?P<card_id>\w+)$', views.concrete_card, name='concrete_card'),
-    url(r'^cards/comments/(?P<card_id>\w+)$', views.comments, name='comments'),
-    url(r'^cards/search/(?P<key>\S+)$', views.search_cards, name='search_cards'),
+    url(r'^boards$', views.BoardsList.as_view(), name='boards_list'),
+    url(r'^boards/(?P<id>\w+)$', views.BoardsDetail.as_view(), name='boards_detail'),
+    url(r'^columns$', views.ColumnsList.as_view(), name='columns_list'),
+    url(r'^columns/(?P<id>\w+)$', views.ColumnsDetail.as_view(), name='columns_detail'),
+    url(r'^cards$', views.CardsList.as_view(), name='cards_list'),
+    url(r'^cards/(?P<id>\w+)$', views.CardsDetail.as_view(), name='cards_detail'),
+    url(r'^comments$', views.CommentsList.as_view(), name='comments_list'),
+    url(r'^comments/(?P<id>\w+)$', views.CommentsDetail.as_view(), name='comments_detail'),
 ]
 
 app_name = 'boards'
